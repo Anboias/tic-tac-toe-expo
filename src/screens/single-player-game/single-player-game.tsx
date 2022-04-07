b6094ca1082b373ee2cb7528911466d9ac7d0338
Bogdan Iasinovschi <biasinovschi@gmail.com>
2022-04-07 13:28:58 +0300

    Add printFormattedBoard function

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -4,12 +4,14 @@ import styles from "./single-player-game.style";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { GradientBackground, Board } from "@components";
+import { printFormattedBoard } from "@utils";
 
 type GameProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "SinglePlayerGame">;
 };
 
 export default function Game({ navigation }: GameProps): ReactElement {
+  printFormattedBoard(["x", "o", null, "x", "o", null, "x", "o", null]);
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
