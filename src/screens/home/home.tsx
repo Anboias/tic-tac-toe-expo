5cd1a3040692e8bb1ce72568f2fed95f4a4d5d9a
Bogdan Iasinovschi <biasinovschi@gmail.com>
2022-04-07 11:22:47 +0300

    Create front page

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,9 +1,10 @@
-import { ScrollView, Text, Button } from "react-native";
+import { ScrollView, Image, View } from "react-native";
 import React from "react";
 import styles from "./home.style";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { GradientBackground } from "@components";
+import { Button } from "@components";
 
 type HomeProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "Home">;
@@ -13,8 +14,16 @@ export default function Home({ navigation }: HomeProps) {
   return (
     <GradientBackground>
       <ScrollView contentContainerStyle={styles.container}>
-        <Text>Home</Text>
-        <Button title="Game" onPress={() => navigation.navigate("Game", { gameId: "455" })} />
+        <Image source={require("@assets/logo.png")} style={styles.logo} />
+        <View style={styles.buttons}>
+          <Button style={styles.button} title="Single Player" />
+          <Button style={styles.button} title="Multiplayer" />
+          <Button style={styles.button} title="Login" />
+          <Button style={styles.button} title="Settings" />
+        </View>
+
+        {/* <Text>Home</Text>
+        <Button title="Game" onPress={() => navigation.navigate("Game", { gameId: "455" })} /> */}
       </ScrollView>
     </GradientBackground>
   );
