aa4923d0e2e66c2ee0de9a44ca4ae6680bd009c7
Bogdan Iasinovschi <biasinovschi@gmail.com>
2022-04-07 13:54:23 +0300

    Add more utils functions

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -4,14 +4,20 @@ import styles from "./single-player-game.style";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
 import { GradientBackground, Board } from "@components";
-import { printFormattedBoard } from "@utils";
+import { printFormattedBoard, isEmpty, isFull, getAvailableMoves, BoardState } from "@utils";
 
 type GameProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "SinglePlayerGame">;
 };
 
 export default function Game({ navigation }: GameProps): ReactElement {
-  printFormattedBoard(["x", "o", null, "x", "o", null, "x", "o", null]);
+  const b: BoardState = ["x", "o", null, "x", "o", null, "x", "o", null];
+
+  printFormattedBoard(b);
+  console.log(isEmpty(b));
+  console.log(isFull(b));
+  console.log(getAvailableMoves(b));
+
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
