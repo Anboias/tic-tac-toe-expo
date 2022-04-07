f3bf054589e288f01e04d56875eeb8168b3b4d27
Bogdan Iasinovschi <biasinovschi@gmail.com>
2022-04-07 12:49:35 +0300

    Create Tic-Tac-Toe Board Component

--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,9 +1,9 @@
-import { Button, Text, SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native";
 import React, { ReactElement } from "react";
 import styles from "./single-player-game.style";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { StackNavigatorParams } from "@config/navigator";
-import { GradientBackground } from "@components";
+import { GradientBackground, Board } from "@components";
 
 type GameProps = {
   navigation: StackNavigationProp<StackNavigatorParams, "SinglePlayerGame">;
@@ -13,8 +13,13 @@ export default function Game({ navigation }: GameProps): ReactElement {
   return (
     <GradientBackground>
       <SafeAreaView style={styles.container}>
-        <Text style={{ color: "#fff" }}>Game</Text>
-        <Button title="Home" onPress={() => navigation.navigate("Home")} />
+        <Board
+          onCellPressed={(index: number) => {
+            alert(index);
+          }}
+          state={["x", "o", null, "x", "o", null, "x", "o", null]}
+          size={300}
+        />
       </SafeAreaView>
     </GradientBackground>
   );
